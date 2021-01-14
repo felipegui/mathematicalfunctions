3b06add6833436847d33821fbb33abaa6f0db531
Felipe Guimarães <fguiengenharia@gmail.com>
2021-01-14 15:45:27 -0300

    Criando um botão de gerador de números aleatórios

--- assets/js/mathematicalfunctions.js
+++ assets/js/mathematicalfunctions.js
@@ -2,6 +2,8 @@
     Usando algumas funções matemáticas.
     Gerador de números aleatórios de 0 a 100
 */
-let generator = Math.floor(Math.random() * 100);
+function generateNumber() {
+    let res = Math.floor(Math.random() * 100);
 
-document.getElementById("mathematic").innerHTML = generator;
+    document.querySelector(".mathematic").innerHTML = res;
+}
