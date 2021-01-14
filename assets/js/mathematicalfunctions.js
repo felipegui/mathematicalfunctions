2f0ac18e2a7f7a45168e4e6c50575b1b4f792f07
Felipe Guimarães <fguiengenharia@gmail.com>
2021-01-14 11:26:24 -0300

    Usando Funções Matemáticas

---
+++ assets/js/mathematicalfunctions.js
@@ -0,0 +1,7 @@
+/*
+    Usando algumas funções matemáticas.
+    Gerador de números aleatórios de 0 a 100
+*/
+let generator = Math.floor(Math.random() * 100);
+
+document.getElementById("mathematic").innerHTML = generator;
